addc5de639745b1c663b6fc45d01c88c44461b22
Hanxun Huang <curtishuang@foxmail.com>
2015-12-09 19:44:53 -0500

    test commit

--- FireUp/Complain/ViewOtherUserProfileTableViewController.swift
+++ FireUp/Complain/ViewOtherUserProfileTableViewController.swift
@@ -19,39 +19,6 @@ class ViewOtherUserProfileTableViewController: UITableViewController {
     var TRUsername:String!
     var targetUser:PFUser!
     
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        usernameTextField.text = TRUsername
-        let usernameQuery = PFQuery(className: "_User")
-        usernameQuery.whereKey("username", equalTo: TRUsername)
-        targetUser = usernameQuery.getFirstObject() as! PFUser
-        if(targetUser.objectForKey("User_Profile_Picture") != nil){
-            let profileImage = targetUser.objectForKey("User_Profile_Picture") as! PFFile
-            profileImage.getDataInBackgroundWithBlock{ (imageData: NSData?, error:NSError?) -> Void in
-                self.profileImageView.image = UIImage(data: imageData!)
-            }
-        }
-        
-        userDataCell.selectionStyle = UITableViewCellSelectionStyle.None
-        let followQuery = PFQuery(className: "FollowRelation")
-        followQuery.whereKey("User", equalTo: PFUser.currentUser())
-        followQuery.whereKey("Target", equalTo: targetUser)
-        followQuery.findObjectsInBackgroundWithBlock { (results:[AnyObject]!, error: NSError!) -> Void in
-            if error == nil{
-                if results.count>0 {
-                    self.followButton.setTitle("Unfollow", forState:  UIControlState.Normal)
-                    self.followButton.backgroundColor = UIColor(red: 0.91, green: 0.43, blue: 0.26, alpha: 1.0)
-                    self.followButton.setTitleColor(UIColor.darkGrayColor(), forState: UIControlState.Normal)
-                }else{
-                    self.followButton.setTitle("Follow", forState:  UIControlState.Normal)
-                    self.followButton.backgroundColor = UIColor(red: 0.2, green: 0.67, blue: 0.85, alpha: 1.0)
-                    self.followButton.setTitleColor(UIColor.darkGrayColor(), forState: UIControlState.Normal)
-                }
-            }
-        }
-        
-
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -96,7 +63,7 @@ class ViewOtherUserProfileTableViewController: UITableViewController {
             if error == nil{
                 if results.count>0 {
                     self.unfollowUer()
-                    NSThread.sleepForTimeInterval(1)
+                    NSThread.sleepForTimeInterval(3)
                     self.viewDidLoad()
                 }else{
                     self.followUser()
@@ -111,7 +78,7 @@ class ViewOtherUserProfileTableViewController: UITableViewController {
         let followRelation = PFObject(className: "FollowRelation")
         followRelation["User"] = PFUser.currentUser()
         followRelation["Target"] = targetUser
-        followRelation.saveInBackgroundWithBlock(nil)
+        followRelation.save()
     }
     func unfollowUer(){
         let followQuery = PFQuery(className: "FollowRelation")
