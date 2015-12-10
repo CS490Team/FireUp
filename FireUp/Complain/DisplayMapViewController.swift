1ac9a0702defa4f134606a92b1aba841f1db8194
go307 <sun311@purdue.edu>
2015-12-09 19:51:27 -0500

    Update Storyboard

---
+++ FireUp/Complain/DisplayMapViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DisplayMapViewController.swift
+//  Fire Up
+//
+//  Created by Kai Sun on 12/9/15.
+//
+//
+
+import UIKit
+
+class DisplayMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
